3155dc757b078caa25378300de5171534a8ffb64
himanshurd <91275023+himanshurd@users.noreply.github.com>
2022-05-26 08:28:55 -0700

    refactor the cook.rb file to get away from stinky long function smell by using extract method technique

---
+++ refactor_cook.rb
@@ -0,0 +1,26 @@
+def cut_vegetables
+  # we will add vegetables in the skillet
+end
+
+def spices
+  # we will add spices in skillet that need to be added in food
+end
+
+def gas_on?
+  # this will return true if the gas is available
+end
+
+def put_skillet_on_gas  
+end
+
+def pay_gas_bill
+  # if gas is not turning on then pay bill
+end
+
+def make_food
+  # steps for making the desired food
+end
+
+def add_spices
+  # steps for adding the required spices into the food 
+end
