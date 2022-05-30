8f3bc0f80eef5580b60c68158a9f156f1b4571bd
himanshurd <91275023+himanshurd@users.noreply.github.com>
2022-05-29 23:12:55 -0700

    Add steps in code.

--- refactor_cook.rb
+++ refactor_cook.rb
@@ -1,3 +1,17 @@
+#Smell: long Function: It's simply a method with a large number of lines of code. 
+#It's debatable how long is too lengthy, because it varies depending on the language. 
+#Sandi Metz, for example, claims that methods should be no more than five lines long in her developer standards.
+
+# Refactor Technique "Extract Function": This refactoring involves moving code from an existing method to a new one. 
+# You'll be able to have smaller methods with more descriptive names as a result of this.
+
+# Step 1: Identify code fragment to extract. 
+# Step 2: Create empty method and copy code. 
+# Step 3: Identify undeclared variables that must be arguments. 
+# Step 4: Identify variables that are still used in old method. 
+# Step 5: Call new method from original method.
+# Step 6: If the new method if working as expected then delete the old code. 
+  
 def cut_vegetables
   # we will add vegetables in the skillet
 end
